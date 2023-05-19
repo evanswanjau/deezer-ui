cff79159da497b75540858265364ef986e9829d8
Evans Wanjau <evanswanjau@gmail.com>
2023-05-19 17:47:31 +0300

    update: changed the keyword to track,  made search bar smaller, fixed, and mobile responsive

--- src/component/searchBar.tsx
+++ src/component/searchBar.tsx
@@ -1,33 +1,39 @@
 import { useState } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import background from "../assets/background.jpg";
 
 export const SearchBar = () => {
-    const [keyword, setKeyword] = useState("keyword");
+    const [keyword, setKeyword] = useState<string>("track");
 
     return (
-        <div className="w-11/12 sm:w-6/12 md:w-4/12 mx-auto">
-            <div className="text-white text-xl font-regular flex justify-center my-5 mx-auto">
-                Search {keyword}
-            </div>
-            <div className="relative">
-                <input
-                    type="email"
-                    id="email"
-                    className="block w-full border-none p-4 outline-none tracking-wide text-sm text-gray-800 border rounded-lg bg-gray-100"
-                    placeholder="Search"
-                    onChange={(event) => {
-                        setKeyword(
-                            event.target.value === ""
-                                ? "keyword"
-                                : event.target.value
-                        );
-                    }}
-                />
-                <button
-                    type="submit"
-                    className="text-gray-800 font-semibold tracking-widest font-inter absolute right-1 bottom-1 bg-gray-300 shadow-lg hover:bg-gray-900 hover:text-white focus:outline-none rounded-lg text-sm px-4 py-[0.9em] transition duration-300 delay-50"
-                >
-                    SUBMIT
-                </button>
+        <div
+            className="fixed w-full lg:w-10/12 pt-2 pb-5"
+            style={{
+                backgroundImage: `url(${background})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+            }}
+        >
+            <div className="w-11/12 sm:w-6/12 md:w-4/12 mx-auto">
+                <div className="text-white text-xl font-regular flex justify-center my-2 mx-auto">
+                    Search {keyword}
+                </div>
+                <div className="relative">
+                    <input
+                        type="email"
+                        id="email"
+                        className="block w-full border-none p-2 outline-none tracking-wide text-sm text-gray-800 border rounded-lg bg-gray-100"
+                        placeholder="Search track"
+                        onChange={(event: any) => {
+                            setKeyword(
+                                event.target.value === ""
+                                    ? "track"
+                                    : event.target.value
+                            );
+                        }}
+                    />
+                    <HiOutlineSearch className="text-2xl text-gray-400 absolute right-1 bottom-1.5" />
+                </div>
             </div>
         </div>
     );
