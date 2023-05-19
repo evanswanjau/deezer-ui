b58a1c2c0649e2362cebb13ec50bae3e83ebee9e
Evans Wanjau <evanswanjau@gmail.com>
2023-05-19 18:14:09 +0300

    update: made mobile page responsive added searchReasults layout

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,17 +1,23 @@
 import background from "../assets/background.jpg";
 import { SearchBar } from "../component/searchBar";
+import { SearchResults } from "../layouts/searchResults";
 
 export const Home = () => {
     return (
         <div
-            className="h-screen overflow-y-auto flex items-center"
+            className="h-screen flex"
             style={{
                 backgroundImage: `url(${background})`,
                 backgroundSize: "cover",
                 backgroundRepeat: "no-repeat",
             }}
         >
-            <SearchBar />
+            <div className="overflow-y-auto flex justify-center w-full">
+                <div className="w-full lg:w-10/12">
+                    <SearchBar />
+                    <SearchResults />
+                </div>
+            </div>
         </div>
     );
 };
