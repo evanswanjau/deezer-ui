1b55113b95503a0977b90aa228e9ce5830063889
Evans Wanjau <evanswanjau@gmail.com>
2023-05-20 03:04:03 +0300

    update: restructured routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home } from "./pages/home";
+import { ArtistPage } from "./pages/artist";
 
 export const App = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/">
-                    <Home />
-                </Route>
+                <Route path="/artist/:name" component={ArtistPage} />
+                <Route exact path="/" component={Home}/>
             </Switch>
         </BrowserRouter>
     );
