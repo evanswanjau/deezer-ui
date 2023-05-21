c9e6d4820de5215198859b219099d979d43f8947
Evans Wanjau <evanswanjau@gmail.com>
2023-05-21 08:54:10 +0300

    update: refactred ui and changed prop parameters

--- src/component/topTracks.tsx
+++ src/component/topTracks.tsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 
 interface Track {
-    name: string;
+    id: number;
+    title: string;
     duration: string;
 }
 
@@ -11,10 +12,10 @@ interface Tracks {
 
 export const TopTracks = ({ tracks }: Tracks) => {
     return (
-        <div className="w-4/12 mx-3 rounded-xl shadow-md shadow-green-400 bg-white p-8">
+        <div className="w-full lg:w-4/12 m-3 rounded-xl shadow-md bg-white p-8">
             <h4 className="text-xl font-bold">Top Tracks</h4>
             <ul className="mt-5">
-                {tracks.map(({ name, duration }: Track, i: number) => {
+                {tracks.map(({ title, duration }: Track, i: number) => {
                     return (
                         <li key={i}>
                             <Link
@@ -25,7 +26,7 @@ export const TopTracks = ({ tracks }: Tracks) => {
                             >
                                 <div className="flex justify-start space-x-8 w-10/12">
                                     <span>{i + 1}</span>
-                                    <p>{name}</p>
+                                    <p>{title}</p>
                                 </div>
 
                                 <p className="text-gray-400 group-hover:text-green-600">
