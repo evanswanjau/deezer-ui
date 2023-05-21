8541faabe0b80ade4f4d4cdc33284b531278ff51
Evans Wanjau <evanswanjau@gmail.com>
2023-05-21 08:56:12 +0300

    new: added integer helper that compacts a long number

---
+++ src/helpers/integers.tsx
@@ -0,0 +1,19 @@
+/**
+ * A function that converst a long number
+ * to the nearest K|M|B|T
+ * @param number 700000
+ * @returns 700K
+ */
+export const compactNumber = (number: number): string | number | undefined => {
+    if (number < 1000) {
+        return number;
+    } else if (number >= 1000 && number < 1_000_000) {
+        return (number / 1000).toFixed(1) + "K";
+    } else if (number >= 1_000_000 && number < 1_000_000_000) {
+        return (number / 1_000_000).toFixed(1) + "M";
+    } else if (number >= 1_000_000_000 && number < 1_000_000_000_000) {
+        return (number / 1_000_000_000).toFixed(1) + "B";
+    } else if (number >= 1_000_000_000_000 && number < 1_000_000_000_000_000) {
+        return (number / 1_000_000_000_000).toFixed(1) + "T";
+    }
+};
