a8a9f4c8317f83ba06cb26dee452b919a9598b25
Evans Wanjau <evanswanjau@gmail.com>
2023-05-21 06:39:16 +0300

    new: integrated search bar with api requests

--- src/component/searchBar.tsx
+++ src/component/searchBar.tsx
@@ -1,9 +1,46 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 import { HiOutlineSearch } from "react-icons/hi";
 import background from "../assets/background.jpg";
+import { useSnackbar } from "notistack";
 
-export const SearchBar = () => {
-    const [keyword, setKeyword] = useState<string>("track");
+export const SearchBar = ({
+    keyword,
+    setKeyword,
+    setData,
+    setLoading,
+}: {
+    keyword: string;
+    setKeyword: any;
+    setData: any;
+    setLoading: any;
+}) => {
+    const { enqueueSnackbar } = useSnackbar();
+
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            if (keyword !== "") {
+                axios
+                    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                    .post(`${process.env.REACT_APP_API_URL}/search/tracks`, {
+                        keyword,
+                    })
+                    .then((response) => {
+                        setData(response.data);
+                    })
+                    .catch((err) => {
+                        enqueueSnackbar(err.message, { variant: "error" });
+                    })
+                    .finally(() => {
+                        setLoading(false);
+                    });
+            }
+        }, 3000);
+
+        return () => {
+            clearTimeout(delayDebounceFn);
+        };
+    }, [keyword]); // eslint-disable-line
 
     return (
         <div
@@ -16,20 +53,19 @@ export const SearchBar = () => {
         >
             <div className="w-11/12 sm:w-6/12 md:w-4/12 mx-auto">
                 <div className="text-white text-xl font-regular flex justify-center my-2 mx-auto">
-                    Search {keyword}
+                    Search {keyword === "" ? "track" : keyword}
                 </div>
                 <div className="relative">
                     <input
                         type="email"
                         id="email"
                         className="block w-full border-none p-2 outline-none tracking-wide text-sm text-gray-800 border rounded-lg bg-gray-100"
-                        placeholder="Search track"
+                        placeholder={`Search ${
+                            keyword === "" ? "track" : keyword
+                        }`}
                         onChange={(event: any) => {
-                            setKeyword(
-                                event.target.value === ""
-                                    ? "track"
-                                    : event.target.value
-                            );
+                            setLoading(event.target.value !== "");
+                            setKeyword(event.target.value);
                         }}
                     />
                     <HiOutlineSearch className="text-2xl text-gray-400 absolute right-1 bottom-1.5" />
