d6743c8c0e9ed11044d3d8e4e2e36e5e02432500
Evans Wanjau <evanswanjau@gmail.com>
2023-05-21 08:53:38 +0300

    update: refactored ui and change prop parameters

--- src/component/album.tsx
+++ src/component/album.tsx
@@ -1,20 +1,22 @@
 export const Album = ({
-    image,
-    name,
-    released,
+    id,
+    title,
+    release_date, // eslint-disable-line
+    cover_medium, // eslint-disable-line
 }: {
-    image: string;
-    name: string;
-    released: string;
+    id: number;
+    title: string;
+    release_date: string;
+    cover_medium: string;
 }) => {
     return (
         <div>
             <img
-                src={`/images${image}`}
+                src={cover_medium}
                 className="rounded-lg shadow-md mb-3 hover:scale-[1.02] transition duration-200 ease-in-out cursor-pointer"
             />
-            <p className="text-sm font-bold text-white">{name}</p>
-            <p className="text-gray-400">{released}</p>
+            <p className="text-sm font-bold text-white">{title}</p>
+            <p className="text-gray-400 mt-0.5">{release_date.split("-")[0]}</p>
         </div>
     );
 };
