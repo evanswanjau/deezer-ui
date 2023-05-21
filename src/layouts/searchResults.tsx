42dfe5cbb292271277e7843f7345b20a2927b2d4
Evans Wanjau <evanswanjau@gmail.com>
2023-05-21 06:38:37 +0300

    update: resfactored ui to meet api requirements

--- src/layouts/searchResults.tsx
+++ src/layouts/searchResults.tsx
@@ -1,247 +1,55 @@
-import { useState } from "react";
 import { Track } from "../component/track";
+import { Loader } from "../component/loader";
 
 interface TrackProp {
-    track: string;
-    artist: string;
+    id: number;
+    title: string;
+    duration: number;
     album: string;
-    duration: string;
-    image: string;
+    artist: string;
+    cover_medium: string;
 }
 
-export const SearchResults = () => {
-    const [data] = useState<TrackProp[]>([
-        {
-            track: "major distribution",
-            artist: "drake",
-            album: "rich flex",
-            duration: "3:59",
-            image: "/her-loss.jpg",
-        },
-        {
-            track: "privileged rappers",
-            artist: "drake",
-            album: "rich flex",
-            duration: "2:40",
-            image: "/her-loss.jpg",
-        },
-        {
-            track: "strangers by nature",
-            artist: "adele",
-            album: "30",
-            duration: "3:02",
-            image: "/30.png",
-        },
-        {
-            track: "easy on me",
-            artist: "adele",
-            album: "30",
-            duration: "3:44",
-            image: "/30.png",
-        },
-        {
-            track: "major distribution",
-            artist: "drake",
-            album: "rich flex",
-            duration: "3:59",
-            image: "/her-loss.jpg",
-        },
-        {
-            track: "privileged rappers",
-            artist: "drake",
-            album: "rich flex",
-            duration: "2:40",
-            image: "/her-loss.jpg",
-        },
-        {
-            track: "strangers by nature",
-            artist: "adele",
-            album: "30",
-            duration: "3:02",
-            image: "/30.png",
-        },
-        {
-            track: "easy on me",
-            artist: "adele",
-            album: "30",
-            duration: "3:44",
-            image: "/30.png",
-        },
-        {
-            track: "strangers by nature",
-            artist: "adele",
-            album: "30",
-            duration: "3:02",
-            image: "/30.png",
-        },
-        {
-            track: "easy on me",
-            artist: "adele",
-            album: "30",
-            duration: "3:44",
-            image: "/30.png",
-        },
-        {
-            track: "major distribution",
-            artist: "drake",
-            album: "rich flex",
-            duration: "3:59",
-            image: "/her-loss.jpg",
-        },
-        {
-            track: "privileged rappers",
-            artist: "drake",
-            album: "rich flex",
-            duration: "2:40",
-            image: "/her-loss.jpg",
-        },
-        {
-            track: "strangers by nature",
-            artist: "adele",
-            album: "30",
-            duration: "3:02",
-            image: "/30.png",
-        },
-        {
-            track: "easy on me",
-            artist: "adele",
-            album: "30",
-            duration: "3:44",
-            image: "/30.png",
-        },
-        {
-            track: "privileged rappers",
-            artist: "drake",
-            album: "rich flex",
-            duration: "2:40",
-            image: "/her-loss.jpg",
-        },
-        {
-            track: "strangers by nature",
-            artist: "adele",
-            album: "30",
-            duration: "3:02",
-            image: "/30.png",
-        },
-        {
-            track: "easy on me",
-            artist: "adele",
-            album: "30",
-            duration: "3:44",
-            image: "/30.png",
-        },
-        {
-            track: "strangers by nature",
-            artist: "adele",
-            album: "30",
-            duration: "3:02",
-            image: "/30.png",
-        },
-        {
-            track: "easy on me",
-            artist: "adele",
-            album: "30",
-            duration: "3:44",
-            image: "/30.png",
-        },
-        {
-            track: "major distribution",
-            artist: "drake",
-            album: "rich flex",
-            duration: "3:59",
-            image: "/her-loss.jpg",
-        },
-        {
-            track: "privileged rappers",
-            artist: "drake",
-            album: "rich flex",
-            duration: "2:40",
-            image: "/her-loss.jpg",
-        },
-        {
-            track: "strangers by nature",
-            artist: "adele",
-            album: "30",
-            duration: "3:02",
-            image: "/30.png",
-        },
-        {
-            track: "easy on me",
-            artist: "adele",
-            album: "30",
-            duration: "3:44",
-            image: "/30.png",
-        },
-        {
-            track: "privileged rappers",
-            artist: "drake",
-            album: "rich flex",
-            duration: "2:40",
-            image: "/her-loss.jpg",
-        },
-        {
-            track: "strangers by nature",
-            artist: "adele",
-            album: "30",
-            duration: "3:02",
-            image: "/30.png",
-        },
-        {
-            track: "easy on me",
-            artist: "adele",
-            album: "30",
-            duration: "3:44",
-            image: "/30.png",
-        },
-        {
-            track: "strangers by nature",
-            artist: "adele",
-            album: "30",
-            duration: "3:02",
-            image: "/30.png",
-        },
-        {
-            track: "easy on me",
-            artist: "adele",
-            album: "30",
-            duration: "3:44",
-            image: "/30.png",
-        },
-        {
-            track: "major distribution",
-            artist: "drake",
-            album: "rich flex",
-            duration: "3:59",
-            image: "/her-loss.jpg",
-        },
-        {
-            track: "privileged rappers",
-            artist: "drake",
-            album: "rich flex",
-            duration: "2:40",
-            image: "/her-loss.jpg",
-        },
-        {
-            track: "strangers by nature",
-            artist: "adele",
-            album: "30",
-            duration: "3:02",
-            image: "/30.png",
-        },
-        {
-            track: "easy on me",
-            artist: "adele",
-            album: "30",
-            duration: "3:44",
-            image: "/30.png",
-        },
-    ]);
-
+export const SearchResults = ({
+    keyword,
+    data,
+    loading,
+}: {
+    keyword: string;
+    data: TrackProp[];
+    loading: boolean;
+}) => {
     return (
-        <div className="my-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 2xl:grid-cols-6 gap-8 mt-32 p-5">
-            {data.map((item, i) => {
-                return <Track key={i} {...item} />;
-            })}
+        <div className="mt-32 p-5">
+            {loading ? (
+                <div className="flex justify-center">
+                    <Loader />
+                </div>
+            ) : (
+                <div>
+                    {data.length > 0 && (
+                        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 2xl:grid-cols-6 gap-8">
+                            {data.map((item, i) => {
+                                return <Track key={i} {...item} />;
+                            })}
+                        </div>
+                    )}
+                    {data.length < 1 && keyword !== "track" && (
+                        <div className="flex justify-center">
+                            <h3 className="text-white text-2xl">
+                                {keyword !== "" ? (
+                                    <span>
+                                        No tracks with the keyword{" "}
+                                        <u>{keyword}</u>
+                                    </span>
+                                ) : (
+                                    <span>Start typing</span>
+                                )}
+                            </h3>
+                        </div>
+                    )}
+                </div>
+            )}
         </div>
     );
 };
