9b76689a571e32704e4fc9e41b232a82aface684
Evans Wanjau <evanswanjau@gmail.com>
2023-05-21 08:56:56 +0300

    update: integratedview artist api

--- src/pages/artist.tsx
+++ src/pages/artist.tsx
@@ -1,71 +1,74 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import background from "../assets/background.jpg";
 import { Album } from "../component/album";
 import { TopTracks } from "../component/topTracks";
 import { ArtistHeader } from "../component/artistHeader";
 import { HiOutlineArrowLeft } from "react-icons/hi";
+import { useSnackbar } from "notistack";
 
 interface Track {
-    name: string;
+    id: number;
+    title: string;
     duration: string;
 }
 
 interface AlbumInterface {
-    name: string;
-    released: string;
-    image: string;
+    id: number;
+    title: string;
+    release_date: string;
+    cover_medium: string;
 }
 
 interface Artist {
+    id: number;
     name: string;
-    fans: number;
+    nb_fan: number;
     bio: string;
-    image: string;
+    picture_medium: string;
 }
 
-export const ArtistPage = () => {
-    const [artist] = useState<Artist>({
-        name: "drake",
-        fans: 382000,
+export const ArtistPage = ({ match: { params } }: any) => {
+    const [artist, setArtist] = useState<Artist>({
+        id: 0,
+        name: "-",
+        nb_fan: 0,
         bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        image: "/her-loss.jpg",
+        picture_medium: "",
     });
 
-    const [tracks] = useState<Track[]>([
-        { name: "Get it together", duration: "4:10" },
-        { name: "Jorja Interlude", duration: "1:47" },
-        { name: "Jumpman", duration: "2:12" },
-        { name: "Controller", duration: "3:39" },
-        { name: "Know yourself", duration: "3:32" },
-    ]);
+    const [tracks, setTracks] = useState<Track[]>([]);
+    const [albums, setAlbums] = useState<AlbumInterface[]>([]);
+    const { enqueueSnackbar } = useSnackbar();
 
-    const [albums] = useState<AlbumInterface[]>([
-        { name: "Take care", released: "2011", image: "/her-loss.jpg" },
-        { name: "Views", released: "2016", image: "/her-loss.jpg" },
-        { name: "Scorpion", released: "2018", image: "/her-loss.jpg" },
-        { name: "More life", released: "2017", image: "/her-loss.jpg" },
-        {
-            name: "Certified lover boy",
-            released: "2021",
-            image: "/her-loss.jpg",
-        },
-        {
-            name: "Certified lover boy",
-            released: "2021",
-            image: "/her-loss.jpg",
-        },
-        {
-            name: "Certified lover boy",
-            released: "2021",
-            image: "/her-loss.jpg",
-        },
-        {
-            name: "Certified lover boy",
-            released: "2021",
-            image: "/her-loss.jpg",
-        },
-    ]);
+    useEffect(() => {
+        const url = process.env.REACT_APP_API_URL;
+        axios
+            .all([
+                // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                axios.get(`${url}/artist/${params.id}`),
+                // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                axios.get(`${url}/artist/${params.id}/top`),
+                // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                axios.get(`${url}/artist/${params.id}/albums`),
+            ])
+            .then(
+                axios.spread((data1, data2, data3) => {
+                    if (data1.data.error !== undefined) {
+                        throw new Error(data1.data.message);
+                    }
+
+                    setArtist({ ...artist, ...data1.data.artist });
+                    setTracks(data2.data.data);
+                    setAlbums(data3.data.data);
+                })
+            )
+            .catch((err) => {
+                console.log(err.message);
+                enqueueSnackbar(err.message, { variant: "error" });
+            });
+    }, []); // eslint-disable-line
 
     return (
         <div
@@ -77,9 +80,15 @@ export const ArtistPage = () => {
             }}
         >
             <div className="overflow-y-auto flex flex-col h-full w-full px-10">
-                <Link to="" className="text-white py-5 flex hover:text-green-600 transition duration-150 ease-in-out w-fit"><HiOutlineArrowLeft className="mx-5 mt-0.2 text-2xl"/>back to search</Link>
+                <Link
+                    to=""
+                    className="text-white py-5 flex hover:text-green-600 transition duration-150 ease-in-out w-fit"
+                >
+                    <HiOutlineArrowLeft className="mx-5 mt-0.2 text-2xl" />
+                    back to search
+                </Link>
 
-                <div className="flex">
+                <div className="flex flex-col lg:flex-row">
                     <ArtistHeader {...artist} />
                     <TopTracks tracks={tracks} />
                 </div>
