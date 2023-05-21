3a8e79c2b42d5cc5b3dec7f20fc560075d57694f
Evans Wanjau <evanswanjau@gmail.com>
2023-05-21 06:38:07 +0300

    new: configured snackbar

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { App } from './App'
+import ReactDOM from "react-dom/client";
+import { SnackbarProvider } from "notistack";
+import "./index.css";
+import { App } from "./App";
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-)
+    document.getElementById("root") as HTMLElement
+);
 root.render(
-    <React.StrictMode>
+    <SnackbarProvider
+        maxSnack={1}
+        autoHideDuration={3000}
+        anchorOrigin={{ horizontal: "center", vertical: "top" }}
+    >
         <App />
-    </React.StrictMode>
-)
+    </SnackbarProvider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
